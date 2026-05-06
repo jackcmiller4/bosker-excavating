bbb1be4b4a0dee278dff5ef1794a01f0746aa756
jackcmiller4 <274131956+jackcmiller4@users.noreply.github.com>
2026-05-05 21:30:05 -0400

    Configure for custom domain boskerexcavating.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jackcmiller4.github.io',
-  base: '/bosker-excavating/',
+  site: 'https://boskerexcavating.com',
   output: 'static',
 });
